31bf2152402f5d92c43c92a308b2e0cdb9d1f2d3
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-10 13:57:46 -1000

    Added Xcode project.

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Jesse Ruiz on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
