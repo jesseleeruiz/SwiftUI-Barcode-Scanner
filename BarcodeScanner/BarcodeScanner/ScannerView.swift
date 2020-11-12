4b9e7f131fa25d02af23fa51f1e8efb06483101a
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-11 16:03:14 -1000

    Made some corrections to the ScannerVC. Added a ScannerView and implemented it in the UI.

---
+++ BarcodeScanner/BarcodeScanner/ScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Jesse Ruiz on 11/11/20.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        
+        func didFind(barcode: String) {
+            print(barcode)
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
+    }
+}
+
+struct ScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannerView()
+    }
+}
